b61928f2e64eb0478280a0ee62a3714d40cafcd4
rootDEV2990 <website.dev@icloud.com>
2020-09-24 19:20:25 -0500

    Added postgresql for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_09_24_121349) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tweeets", force: :cascade do |t|
     t.text "tweeet"
     t.datetime "created_at", precision: 6, null: false
